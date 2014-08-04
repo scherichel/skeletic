5988dee97c12d57667c8dc2363135d00dcf71294
Stanley Cherichel <scherichel@gmail.com>
2014-08-03 20:42:33 -0400

    Removed JavaScript from enqueue-scripts.php
    JavaScript causing maximum function nesting error

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -6,19 +6,20 @@
     CSS
     =============*/
 
-    wp_enqueue_style( 'style', get_stylesheet_uri() );
-    wp_enqueue_style( 'skeleton-style', get_template_directory_uri() . '/css/skeleton.css');
-    wp_enqueue_style( 'skeleton-base', get_template_directory_uri() . '/css/base.css' );
-    wp_enqueue_style( 'skeleton-layout', get_template_directory_uri() . '/css/layout.css' );
+    wp_register_style( 'style', get_stylesheet_uri() );
+    wp_register_style( 'skeleton-style', get_template_directory_uri() . '/css/skeleton.css');
+    wp_register_style( 'skeleton-base', get_template_directory_uri() . '/css/base.css' );
+    wp_register_style( 'skeleton-layout', get_template_directory_uri() . '/css/layout.css' );
+
+
+    wp_enqueue_style('style');
+    wp_enqueue_style('skeleton-style');
+    wp_enqueue_style('skeleton-base');
+    wp_enqueue_style('skeleton-layout');
 
     /* =======
     javaScript
     =============*/
-
-    wp_register_script( 'core', get_template_directory_uri() . '/js/core.js', array( 'javascript' ), '1.0.0', false );
-    
-    wp_enqueue_script('jquery');
-    wp_enqueue_scripts('core');
   }
 
   add_action( 'wp_enqueue_scripts', 'skeletic_scripts' );
