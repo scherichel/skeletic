6f54f67a8ea6530fcf7d37880444cb316aea9cfc
Stanley Cherichel <scherichel@gmail.com>
2014-08-04 10:32:22 -0400

    Update single.php

--- single.php
+++ single.php
@@ -1,33 +1,47 @@
 <?php get_header(); ?>
-
-	<div class="page">
 		
 		<?php if (have_posts()) : ?>
 		<?php while (have_posts()) : the_post(); ?>
-			
-			<h2 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 
-			<div id="author">
-				Written by <?php the_author_posts_link(); ?>
+		<div class="container">
+			<div class="eleven columns">
+				<article <?php post_class(); ?> >
+					
+					<h2 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+
+					<div id="author">
+						Written by <?php the_author_posts_link(); ?>
+					</div>
+
+					<?php if ( has_post_thumbnail() ) : ?>
+					<div class="post-thumb">
+						<a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
+					</div>
+					<?php endif; ?>
+					<?php the_content(); ?>
+
+					<div class="meta">
+						<ul>
+							<li>Posted  <a href="<?php the_permalink() ?>"><?php the_time('m M Y') ?></a></li>
+							<li>Posted in <?php the_category(','); ?></li>
+						</ul>
+					</div>
+				</article>
+
+				<div>
+					<?php comments_template(); ?>
+				</div>
 			</div>
-
-			<?php if ( has_post_thumbnail() ) : ?>
-			<div class="post-thumb">
-				<a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
-			</div>
-			<?php endif; ?>
-			<?php the_content(); ?>
-
-			<div class="meta">
-				<ul>
-					<li>Posted  <a href="<?php the_permalink() ?>"><?php the_time('m M Y') ?></a></li>
-					<li>Posted in <?php the_category(','); ?></li>
-				</ul>
-			</div>
-			<div class="clear"></div>
-
-			<?php comments_template(); ?>
-
+			<div class="five columns">
+				<p>Sidebar will go here</p>
+				<p>Sidebar will go here</p>
+				<p>Sidebar will go here</p>
+				<p>Sidebar will go here</p>
+				<p>Sidebar will go here</p>
+				<p>Sidebar will go here</p>
+				<p>Sidebar will go here</p>
+			</div>	
+		</div>
 		<?php endwhile; ?>
 		<?php endif; ?>
 	</div>
