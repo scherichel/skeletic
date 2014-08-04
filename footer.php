f3bde5ff458c93a7596a6d27f523fd89c4a437b6
Stanley Cherichel <scherichel@gmail.com>
2014-08-03 21:11:31 -0400

    Added legal div
    Post all legal content (terms)

--- footer.php
+++ footer.php
@@ -1,12 +1,19 @@
 
 		<footer class="container">		
-					<div class="eight columns">
-						Copyright <?php echo date('Y'); ?>
-					</div>
-					<div class="eight columns">
-						<a target="_blank" href="http://scherichel.com">Theme by scherichel</a>
-					</div>
-				</footer>
-	 		<?php wp_footer(); ?>
+			<div class="sixteen columns">
+				<p>
+					Footer sidebar here
+				</p>
+			</div>
+		</footer>
+		<div class="legal container">
+			<div class="eight columns">
+				Copyright <?php echo date('Y'); ?> (Legal goes here)
+			</div>
+			<div class="eight">
+				<a target="_blank" href="http://scherichel.com">Theme by scherichel</a>				
+			</div>
+		</div>
+	 	<?php wp_footer(); ?>
 	</body>
 </html>
