87c83c49e71c2ccf407e40f32d255f824b0e0a30
scherichel <scherichel@gmail.com>
2014-08-10 22:48:03 -0400

    Please enter the commit message for your changes. Lines starting

--- library/navigation.php
+++ library/navigation.php
@@ -1,62 +1,37 @@
 <?php
 
-/**
- * Register Menus
- * http://codex.wordpress.org/Function_Reference/register_nav_menus#Examples
- */
-register_nav_menus(array(
-    'header-menu' => 'Header', // registers the menu in the WordPress admin menu editor
-    'footer-menu' => 'Footer',
-));
-
-
-/**
- * HEADER
- * http://codex.wordpress.org/Function_Reference/wp_nav_menu
- */
-function header_menu() {
-    wp_nav_menu(array(
-        'theme_location'  => '',
-        'menu'            => '',
-        'container'       => 'div',
-        'container_class' => '',
-        'container_id'    => '',
-        'menu_class'      => 'menu',
-        'menu_id'         => '',
-        'echo'            => true,
-        'fallback_cb'     => 'wp_page_menu',
-        'before'          => '',
-        'after'           => '',
-        'link_before'     => '',
-        'link_after'      => '',
-        'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-        'depth'           => 0,
-        'walker'          => ''
+    /**
+     * Register Menus
+     * http://codex.wordpress.org/Function_Reference/register_nav_menus#Examples
+     */
+    register_nav_menus(array(
+        'header-menu' => 'Header' // registers the menu in the WordPress admin menu editor
     ));
-}
 
-/**
- * FOOTER
- */
-function footer_menu() {
-    wp_nav_menu(array(
-        'theme_location'  => '',
-        'menu'            => '',
-        'container'       => 'div',
-        'container_class' => '',
-        'container_id'    => '',
-        'menu_class'      => 'menu',
-        'menu_id'         => '',
-        'echo'            => true,
-        'fallback_cb'     => 'wp_page_menu',
-        'before'          => '',
-        'after'           => '',
-        'link_before'     => '',
-        'link_after'      => '',
-        'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-        'depth'           => 0,
-        'walker'          => ''
-    ));
-}
+
+    /**
+     * HEADER
+     * http://codex.wordpress.org/Function_Reference/wp_nav_menu
+     */
+    function header_menu() {
+        wp_nav_menu(array(
+            'theme_location'  => '',
+            'menu'            => '',
+            'container'       => 'div',
+            'container_class' => '',
+            'container_id'    => '',
+            'menu_class'      => 'menu',
+            'menu_id'         => '',
+            'echo'            => true,
+            'fallback_cb'     => 'wp_page_menu',
+            'before'          => '',
+            'after'           => '',
+            'link_before'     => '',
+            'link_after'      => '',
+            'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+            'depth'           => 0,
+            'walker'          => ''
+        ));
+    }
 
 ?>
