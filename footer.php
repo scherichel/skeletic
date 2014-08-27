ce633709cd433a53cb1f952656457029b444f9c2
scherichel <scherichel@gmail.com>
2014-08-27 05:40:38 -0400

    footer.php css changes

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 			<?php dynamic_sidebar("footer-widgets"); ?>
 		</footer>
 		<div class="legal container">
-			<div class="eight columns">
+			<div class="text-left eight columns">
 				Copyright <?php echo date('Y'); ?> (Legal goes here)
 			</div>
 			<div class="text-right eight">
