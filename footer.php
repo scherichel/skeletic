fff699934454df03eb0a8d82314de95ee11cf7ad
Stanley Cherichel <scherichel@gmail.com>
2014-10-20 20:53:00 -0400

    removed copyright info // new file Gruntfile.js

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 		</footer>
 		<div class="legal container">
 			<div class="text-left eight columns">
-				Copyright <?php echo date('Y'); ?> (Legal goes here)
+				Copyright <?php echo date('Y'); ?>
 			</div>
 			<div class="text-right eight">
 				<a target="_blank" href="http://scherichel.com">Theme by scherichel</a>				
