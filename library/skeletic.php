6bd6a3bd483ef7457af5ae84da6511bbcf31b3a4
Stanley Cherichel <scherichel@gmail.com>
2015-02-20 19:52:51 -0500

    Update wp_title()

--- library/skeletic.php
+++ library/skeletic.php
@@ -31,4 +31,28 @@
 			echo '</div><!--// end .pagination -->';
 		}
 	}
+
+	// A better title
+	// http://www.deluxeblogtips.com/2012/03/better-title-meta-tag.html
+	function rw_title( $title, $sep, $seplocation ) {
+	  global $page, $paged;
+	  // Don't affect in feeds.
+	  if ( is_feed() ) return $title;
+	  // Add the blog's name
+	  if ( 'right' == $seplocation ) {
+	    $title .= get_bloginfo( 'name' );
+	  } else {
+	    $title = get_bloginfo( 'name' ) . $title;
+	  }
+	  // Add the blog description for the home/front page.
+	  $site_description = get_bloginfo( 'description', 'display' );
+	  if ( $site_description && ( is_home() || is_front_page() ) ) {
+	    $title .= " {$sep} {$site_description}";
+	  }
+	  // Add a page number if necessary:
+	  if ( $paged >= 2 || $page >= 2 ) {
+	    $title .= " {$sep} " . sprintf( __( 'Page %s', 'dbt' ), max( $paged, $page ) );
+	  }
+	  return $title;
+	} // end better title
 ?>
